24730db50f064d177e55bc71498b112a3bf2537e
C311-23\User <iotmanzsu@gmail.com>
2026-03-12 10:59:39 +0800

    變數介紹

---
+++ C_楊宗祐/0312/numberOne.c
@@ -0,0 +1,12 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+	int num=12700;
+	printf("%d is a an integer\n",num);
+	printf("Hello C!\n");
+	printf("Hello World!\n");
+	system("pause");
+	return 0;
+}
