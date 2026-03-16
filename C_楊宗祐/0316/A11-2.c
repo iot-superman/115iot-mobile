40df9e203dd700d1923596afda9e0197151bd970
C311-23\User <iotmanzsu@gmail.com>
2026-03-16 13:45:43 +0800

    ADD A11-2 c code

---
+++ C_楊宗祐/0316/A11-2.c
@@ -0,0 +1,20 @@
+// https://youtu.be/IlmBwG-Zr-0
+//¤@¹Bºâ¤l 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+   
+ 
+   int a=0;
+   int b=7;
+   int c=-1;
+   printf("a=%d, !a=%d\n",a ,!a);
+   printf("a=%d, !b=%d\n",b ,!b);
+   printf("a=%d, !c1=%d\n",c ,!c);
+   
+   
+   system("pause");
+   return 0;
+}
