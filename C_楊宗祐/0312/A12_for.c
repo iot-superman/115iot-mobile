243eb26c2aff834129570fc71b0ac550febad7b2
C311-07\User <@>
2026-03-20 15:40:43 +0800

    ADD LINUX YOUTUBe

--- C_楊宗祐/0312/A12_for.c
+++
@@ -1,9 +0,0 @@
-
-/*EX1: ½Ð¦C¦L¥X "Hello Every One, Welcome to the C World!"  */
-#include <stdlib.h>
-#include <stdio.h>
-int main(void)
-{
-	printf("Hello Every One, Welcome to the C World!\n");
-	system("pause");
-}
