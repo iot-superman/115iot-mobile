87351370c5818940b213c83deecfea4b6c34ede8
C311-07\User <@>
2026-03-31 10:02:38 +0800

    add c source codes

--- C/0331/A05.c
+++ C/0331/A05.c
@@ -1,3 +1,19 @@
 //
 // Created by User on 2026/3/31.
 //
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    int num1,num2,num3,num4,num5;
+    printf("Please input the scores of 5 students:");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    scanf("%d", &num3);
+    scanf("%d", &num4);
+    scanf("%d", &num5);
+    printf("num1=%d, \nnum2=%d, \nnum3=%d, \nnum4=%d, \nnum5=%d\n", num1,num2,num3,num4,num5);
+
+    system("pause");
+    return 0;
+}
