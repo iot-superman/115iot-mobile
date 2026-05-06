79194311463336caff6b9610012796e5046849ea
SCHOOL <@>
2026-05-06 14:54:58 +0800

    feat: add Blink example sketch with serial initialization and logging

--- Digital_logic/0506/arduino_project/Blink/Blink.ino
+++ Digital_logic/0506/arduino_project/Blink/Blink.ino
@@ -26,6 +26,8 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Blink test");
 }
 
 // the loop function runs over and over again forever
