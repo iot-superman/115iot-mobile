baaf083bd9621b82a3167903cf44492b71feb96d
SCHOOL <@>
2026-05-11 14:56:44 +0800

    ADD MYSQL files

---
+++ MySQL/0511/2026-05-11.sql
@@ -0,0 +1,36 @@
+-- SQL = Stucture Query LAnguage結構化東詢語言)
+
+-- ISO / ANSI 92
+
+#新增/刪除/修改／查詢 
+
+-- table 資料表alter ->欄位coulumn(屬性) /(索引index /外鍵foregion key /觸發器 trigger)
+-- views 檢視表
+-- stored porfedrue 預存程式alter
+-- function函式alter
+
+-- cmdev資料庫alter
+   -- dept=depatmen 部門表
+   -- emp = emplyee 員工表 
+   -- travel = 員工出差alter
+   
+-- world資料庫
+  --city 城市資料表 
+  --country國家資料表
+  --countrylangguage 國家語言資料表
+
+--ci   case insenstitive 不分大小寫
+--cs   case  senstitivre  分大小寫
+  
+ 
+CREATE SCHEMA DBTEST  -- DDL DATADEFIN LANGUAGe:
+show character set;  --顯示系統有什麼編馬可以用
+
+SHOW COLLATION;
+SHOW COLLATION LIKE '%big5%'    -- LIKE 模糊比對
+
+CREATE DATABASE TEST1 CHARACTER SET big5 COLLATE big5_chinese_ci;
+CREATE SCHEMA IF NOT EXISTS TEST1;   -- DDL
+Alter DATABASE TEST1 CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci;  -- DDL
+
+
---
+++ MySQL/0511/homework.sql
@@ -0,0 +1,10 @@
+-- #02 建立一個名稱為DB1的資料庫
+-- 朱星念
+-- •
+-- 下午2:46
+-- 編碼為big5，編碼排序為big5_bin
+CREATE DATABASE DB1 CHARACTER SET big5 COLLATE big5_bin;
+
+
+-- #01 請用指令建立一個名稱為TESTDB的資料庫 
+CREATE SCHEMA IF NOT EXISTS TESTDB;   -- DDL
