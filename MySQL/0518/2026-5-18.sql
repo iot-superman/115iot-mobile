59e8d63474f09977e1f74f018dfc926966f28b27
SCHOOL <@>
2026-05-18 11:18:45 +0800

    文件：添加和更新 MySQL 0518 練習 SQL 文件
    添加新 HW06-09.sql 文件，內容包括 4 個 SQL 練習集，使用 world 和 cmdev 資料庫
    更新 2026-5-18.sql 文件，內容包括額外查詢範例、第二章筆記和練習解答

--- MySQL/0518/2026-5-18.sql
+++ MySQL/0518/2026-5-18.sql
@@ -1,3 +1,5 @@
+-- **相對於書的CHAP2 **
+
 -- 切換到 WORLD 資料庫
 USE WORLD;
 
@@ -55,7 +57,45 @@ SELECT CODE, NAME, Continent FROM WORLD.country WHERE NAME LIKE '%G%' ORDER BY N
 -- 查詢 WORLD.CITY 資料表中，國家代碼為 TWN 且城市名稱不是以 T 開頭的資料
 SELECT * FROM WORLD.CITY WHERE CountryCode = 'TWN' AND NAME NOT LIKE 'T%';
 
+-- 查詢員工姓名、月薪與年資，並依年資排序
+SELECT ENAME,salary,salary*12 AS ANNUALALARY FROM cmdev.EMP ORDER BY ANNUALALARY;
 
+-- 查詢員工姓名、月薪與年資，並依月薪排序
 
+--        1  ,2    ,3    ,so 2 is salary
+SELECT ENAME,salary,salary*12 AS ANNUALALARY FROM cmdev.EMP ORDER BY 2;  
+
+-- mysql 特有LIMIT指令，查詢第10筆到第15筆資料
+SELECT * FROM WORLD LIMIT 10,5;
+
+-- 查詢員工編號、姓名與月薪，並依月薪由大到小排序，並只顯示前3名
+SELECT EMPNO,ENAME,salary FROM cmdev.EMP ORDER by salary DESC LIMIT 3;
+
+-- 查詢員工編號、姓名與月薪，並依月薪由小到大排序，並只顯示前3名
+SELECT EMPNO,ENAME,salary FROM cmdev.EMP ORDER by salary ASC LIMIT 3;
+-- [ALL | DISTINCT]
+-- ALL 指令，查詢所有員工的姓名，並包含重複值
+SELECT ALL ENAME FROM CMDEV.EMP;
+
+-- DISTINCT 指令，查詢所有員工的職稱，並排除重複值
+SELECT DISTINCT JOB FROM CMDEV.EMP;
+
+-- DISTINCT 指令，查詢所有國家的政府形式，並排除重複值
+SELECT DISTINCT GovernmentForm FROM WORLD.country;
+
+SELECT DISTINCT Continent FROM WORLD.country ORDER BY Continent;
+
+-- **PDF 習題**
+-- 1
+-- 查詢國家代碼、國家名稱與人口，並篩選人口大於 5000000 的國家
+SELECT CODE,NAME,Population FROM WORLD.country WHERE Population > 5000000;
+
+-- 2
+-- 查詢洲別、國家代碼與國家名稱，並篩選洲別為 EUROPE、ASIA、AFRICA 的國家
+SELECT Continent,CODE,NAME FROM world.country WHERE Continent IN("EUROPE","ASIA","AFRICA");
+
+-- 3
+-- 查詢國家代碼、國家名稱與人口，並篩選洲別為 MIDDLE EAST 的國家，並依人口由小到大排序
+SELECT CODE,NAME,Population FROM WORLD.country WHERE REGION ="MIDDLE EAST" ORDER BY Population ; 
 
 
