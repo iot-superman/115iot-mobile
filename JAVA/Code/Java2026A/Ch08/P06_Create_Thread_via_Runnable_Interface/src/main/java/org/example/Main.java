029a9d124a80a0725b9bad5cd8bc6abb7f2c35b1
SCHOOL <@>
2026-05-21 16:04:15 +0800

    ADD CODE

--- JAVA/Code/Java2026A/Ch08/P06_Create_Thread_via_Runnable_Interface/src/main/java/org/example/Main.java
+++ JAVA/Code/Java2026A/Ch08/P06_Create_Thread_via_Runnable_Interface/src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
         totoiseThread.start();
         Thread rabbitRunnable = new Thread(new RabbitRunnable());
         rabbitRunnable.start();
+        System.out.println("Main Thread Ends. 主線程繼續執行其他任務...");
 
       
     }
@@ -24,9 +25,10 @@ class  ToroiseRunnale implements Runnable{
 
     @Override
     public void run() {
-        for(int i=0; i<=10; i++){
+        for(int i=0; i<=1000; i++){
             System.out.printf("鳥龜共跑 %d公里\n",i);
         }
+        System.out.println("Toroise Thread Ends.鳥龜跑完了，開始睡覺...");
     }
 }
 
@@ -34,8 +36,9 @@ class  RabbitRunnable implements Runnable{
 
     @Override
     public void run() {
-        for(int i=0;i<=10;i++){
+        for(int i=0;i<=1000;i++){
             System.out.printf("兔子共跑 %d公里\n",i);
         }
+        System.out.println("Rabbitt Thread Ends.兔子跑完了，開始睡覺...");
     }
 }
