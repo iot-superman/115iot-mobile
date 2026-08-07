e326b3bb3a51d2688e88367eb0cf8dbf9d41ad28
SCHOOL <@>
2026-08-07 10:53:00 +0800

    ADD 0807 WEB CODES

---
+++ Web/0807/code_A.js
@@ -0,0 +1,17 @@
+
+ /*
+
+
+
+
+
+
+
+ */
+
+
+
+let access = document.getElementById("code5");  // 1. 選取元素
+let code = access.innerHTML;                    // 2. 讀取內容
+code = code + " midnight";                      // 3. 改變變數 (但 DOM 沒變!)
+alert(code);                                    // 4. 顯示變數內容                                              
