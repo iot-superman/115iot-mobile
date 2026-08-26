61bd9521efa3612b3ce2ddea74950c2ed68c497b
SCHOOL <@>
2026-08-26 16:34:31 +0800

    0826 MIX1

---
+++ 綜合應用(一)/0826/A01/A01.ino
@@ -0,0 +1,16 @@
+int n;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  for(n=0;n<255 ;n++)
+  {
+    Serial.write(n);
+    delay(100);
+  }
+}
